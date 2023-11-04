9241e6017208085db4856f522f545d0713992457
Ish Mahajan <ishmahajanbhanu@gmail.com>
2023-11-04 22:44:38 +0530

    update firebase configs

--- src/examples/Navbars/DashboardNavbar/index.js
+++ src/examples/Navbars/DashboardNavbar/index.js
@@ -1,20 +1,20 @@
 /**
-=========================================================
-* BalAsha - Nurture - v4.0.2
-=========================================================
+ =========================================================
+ * BalAsha - Nurture - v4.0.2
+ =========================================================
 
-* Product Page: https://balasha-nurture.web.app/product/soft-ui-dashboard-react
-* Copyright 2024 BalAsha - Nurture (https://balasha-nurture.web.app)
+ * Product Page: https://balasha-nurture.web.app/product/soft-ui-dashboard-react
+ * Copyright 2024 BalAsha - Nurture (https://balasha-nurture.web.app)
 
-=========================================================
+ =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
 
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 // react-router components
-import { useLocation, Link } from "react-router-dom";
+import {useLocation, Link} from "react-router-dom";
 
 // prop-types is a library for typechecking of props.
 import PropTypes from "prop-types";
@@ -37,20 +37,20 @@ import NotificationItem from "examples/Items/NotificationItem";
 
 // Custom styles for DashboardNavbar
 import {
-  navbar,
-  navbarContainer,
-  navbarRow,
-  navbarIconButton,
-  navbarDesktopMenu,
-  navbarMobileMenu,
+    navbar,
+    navbarContainer,
+    navbarRow,
+    navbarIconButton,
+    navbarDesktopMenu,
+    navbarMobileMenu,
 } from "examples/Navbars/DashboardNavbar/styles";
 
 // BalAsha - Nurture context
 import {
-  useSoftUIController,
-  setTransparentNavbar,
-  setMiniSidenav,
-  setOpenConfigurator,
+    useSoftUIController,
+    setTransparentNavbar,
+    setMiniSidenav,
+    setOpenConfigurator,
 } from "context";
 
 // Images
@@ -58,178 +58,188 @@ import team2 from "assets/images/team-2.jpg";
 import logoSpotify from "assets/images/small-logos/logo-spotify.svg";
 import {auth} from "../../../platform/firebase";
 import LoginScreen from "../../../custom/LoginScreen";
+import Avatar from "../../../assets/theme/components/avatar";
+import SoftAvatar from "../../../components/SoftAvatar";
 
-function DashboardNavbar({ absolute, light, isMini }) {
-  const [navbarType, setNavbarType] = useState();
-  const [controller, dispatch] = useSoftUIController();
-  const { miniSidenav, transparentNavbar, fixedNavbar, openConfigurator } = controller;
-  const [openMenu, setOpenMenu] = useState(false);
-  const route = useLocation().pathname.split("/").slice(1);
+function DashboardNavbar({absolute, light, isMini}) {
+    const [navbarType, setNavbarType] = useState();
+    const [controller, dispatch] = useSoftUIController();
+    const {miniSidenav, transparentNavbar, fixedNavbar, openConfigurator} = controller;
+    const [openMenu, setOpenMenu] = useState(false);
+    const route = useLocation().pathname.split("/").slice(1);
 
-  useEffect(() => {
-    // Setting the navbar type
-    if (fixedNavbar) {
-      setNavbarType("sticky");
-    } else {
-      setNavbarType("static");
-    }
-
-    // A function that sets the transparent state of the navbar.
-    function handleTransparentNavbar() {
-      setTransparentNavbar(dispatch, (fixedNavbar && window.scrollY === 0) || !fixedNavbar);
-    }
-
-    /** 
-     The event listener that's calling the handleTransparentNavbar function when 
-     scrolling the window.
-    */
-    window.addEventListener("scroll", handleTransparentNavbar);
-
-    // Call the handleTransparentNavbar function to set the state with the initial value.
-    handleTransparentNavbar();
-
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("scroll", handleTransparentNavbar);
-  }, [dispatch, fixedNavbar]);
-
-  const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav);
-  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
-  const handleOpenMenu = (event) => setOpenMenu(event.currentTarget);
-  const handleCloseMenu = () => setOpenMenu(false);
-
-  // Render the notifications menu
-  const renderMenu = () => (
-    <Menu
-      anchorEl={openMenu}
-      anchorReference={null}
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "left",
-      }}
-      open={Boolean(openMenu)}
-      onClose={handleCloseMenu}
-      sx={{ mt: 2 }}
-    >
-      <NotificationItem
-        image={<img src={team2} alt="person" />}
-        title={["New message", "from Laur"]}
-        date="13 minutes ago"
-        onClick={handleCloseMenu}
-      />
-      <NotificationItem
-        image={<img src={logoSpotify} alt="person" />}
-        title={["New album", "by Travis Scott"]}
-        date="1 day"
-        onClick={handleCloseMenu}
-      />
-      <NotificationItem
-        color="secondary"
-        image={
-          <Icon fontSize="small" sx={{ color: ({ palette: { white } }) => white.main }}>
-            payment
-          </Icon>
+    useEffect(() => {
+        // Setting the navbar type
+        if (fixedNavbar) {
+            setNavbarType("sticky");
+        } else {
+            setNavbarType("static");
         }
-        title={["", "Payment successfully completed"]}
-        date="2 days"
-        onClick={handleCloseMenu}
-      />
-    </Menu>
-  );
 
-  function signOutUser() {
-    auth.signOut().then(r =>  <LoginScreen/>);
-  }
+        // A function that sets the transparent state of the navbar.
+        function handleTransparentNavbar() {
+            setTransparentNavbar(dispatch, (fixedNavbar && window.scrollY === 0) || !fixedNavbar);
+        }
 
-  return (
-    <AppBar
-      position={absolute ? "absolute" : navbarType}
-      color="inherit"
-      sx={(theme) => navbar(theme, { transparentNavbar, absolute, light })}
-    >
-      <Toolbar sx={(theme) => navbarContainer(theme)}>
-        <SoftBox color="inherit" mb={{ xs: 1, md: 0 }} sx={(theme) => navbarRow(theme, { isMini })}>
-          <Breadcrumbs icon="home" title={route[route.length - 1]} route={route} light={light} />
-          <Icon fontSize="medium" sx={navbarDesktopMenu} onClick={handleMiniSidenav}>
-            {miniSidenav ? "menu_open" : "menu"}
-          </Icon>
-        </SoftBox>
-        {isMini ? null : (
-          <SoftBox sx={(theme) => navbarRow(theme, { isMini })}>
-            <SoftBox pr={1}>
-              <SoftInput
-                placeholder="Type here..."
-                icon={{ component: "search", direction: "left" }}
-              />
-            </SoftBox>
-            <SoftBox color={light ? "white" : "inherit"}>
-              <Link to="/authentication/sign-in/basic">
-                <IconButton sx={navbarIconButton} size="small" onClick={signOutUser}>
-                  <Icon
-                    sx={({ palette: { dark, white } }) => ({
-                      color: light ? white.main : dark.main,
-                    })}
-                  >
-                    account_circle
-                  </Icon>
-                  <SoftTypography
-                    variant="button"
-                    fontWeight="medium"
-                    color={light ? "white" : "dark"}
-                  >
-                    Sign Out
-                  </SoftTypography>
-                </IconButton>
-              </Link>
-              <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarMobileMenu}
-                onClick={handleMiniSidenav}
-              >
-                <Icon className={light ? "text-white" : "text-dark"}>
-                  {miniSidenav ? "menu_open" : "menu"}
-                </Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarIconButton}
-                onClick={handleConfiguratorOpen}
-              >
-                <Icon>settings</Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarIconButton}
-                aria-controls="notification-menu"
-                aria-haspopup="true"
-                variant="contained"
-                onClick={handleOpenMenu}
-              >
-                <Icon className={light ? "text-white" : "text-dark"}>notifications</Icon>
-              </IconButton>
-              {renderMenu()}
-            </SoftBox>
-          </SoftBox>
-        )}
-      </Toolbar>
-    </AppBar>
-  );
+        /**
+         The event listener that's calling the handleTransparentNavbar function when
+         scrolling the window.
+         */
+        window.addEventListener("scroll", handleTransparentNavbar);
+
+        // Call the handleTransparentNavbar function to set the state with the initial value.
+        handleTransparentNavbar();
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("scroll", handleTransparentNavbar);
+    }, [dispatch, fixedNavbar]);
+
+    const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav);
+    const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+    const handleOpenMenu = (event) => setOpenMenu(event.currentTarget);
+    const handleCloseMenu = () => setOpenMenu(false);
+
+    // Render the notifications menu
+    const renderMenu = () => (
+        <Menu
+            anchorEl={openMenu}
+            anchorReference={null}
+            anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+            }}
+            open={Boolean(openMenu)}
+            onClose={handleCloseMenu}
+            sx={{mt: 2}}
+        >
+            <NotificationItem
+                image={<img src={team2} alt="person"/>}
+                title={["New message", "from Laur"]}
+                date="13 minutes ago"
+                onClick={handleCloseMenu}
+            />
+            <NotificationItem
+                image={<img src={logoSpotify} alt="person"/>}
+                title={["New album", "by Travis Scott"]}
+                date="1 day"
+                onClick={handleCloseMenu}
+            />
+            <NotificationItem
+                color="secondary"
+                image={
+                    <Icon fontSize="small" sx={{color: ({palette: {white}}) => white.main}}>
+                        payment
+                    </Icon>
+                }
+                title={["", "Payment successfully completed"]}
+                date="2 days"
+                onClick={handleCloseMenu}
+            />
+        </Menu>
+    );
+
+    function signOutUser() {
+        auth.signOut().then(r => <LoginScreen/>);
+    }
+
+    const DisplayImage = () => {
+        if (auth.currentUser && auth.currentUser.photoURL) {
+          return <SoftAvatar src={auth.currentUser.photoURL} alt="Avatar"/>
+        } else {
+            return (<Icon
+                sx={({palette: {dark, white}}) => ({
+                    color: light ? white.main : dark.main,
+                })}
+            >
+                account_circle
+            </Icon>)
+        }
+    };
+    return (
+        <AppBar
+            position={absolute ? "absolute" : navbarType}
+            color="inherit"
+            sx={(theme) => navbar(theme, {transparentNavbar, absolute, light})}
+        >
+            <Toolbar sx={(theme) => navbarContainer(theme)}>
+                <SoftBox color="inherit" mb={{xs: 1, md: 0}} sx={(theme) => navbarRow(theme, {isMini})}>
+                    <Breadcrumbs icon="home" title={route[route.length - 1]} route={route} light={light}/>
+                    <Icon fontSize="medium" sx={navbarDesktopMenu} onClick={handleMiniSidenav}>
+                        {miniSidenav ? "menu_open" : "menu"}
+                    </Icon>
+                </SoftBox>
+                {isMini ? null : (
+                    <SoftBox sx={(theme) => navbarRow(theme, {isMini})}>
+                        <SoftBox pr={1}>
+                            <SoftInput
+                                placeholder="Type here..."
+                                icon={{component: "search", direction: "left"}}
+                            />
+                        </SoftBox>
+                        <SoftBox color={light ? "white" : "inherit"}>
+                            <Link to="/authentication/sign-in/basic">
+                                <IconButton sx={navbarIconButton} size="small" onClick={signOutUser}>
+
+                                  <DisplayImage/>
+                                    <SoftTypography
+                                        variant="button"
+                                        fontWeight="medium"
+                                        color={light ? "white" : "dark"}
+                                    >
+                                        Sign Out
+                                    </SoftTypography>
+                                </IconButton>
+                            </Link>
+                            <IconButton
+                                size="small"
+                                color="inherit"
+                                sx={navbarMobileMenu}
+                                onClick={handleMiniSidenav}
+                            >
+                                <Icon className={light ? "text-white" : "text-dark"}>
+                                    {miniSidenav ? "menu_open" : "menu"}
+                                </Icon>
+                            </IconButton>
+                            <IconButton
+                                size="small"
+                                color="inherit"
+                                sx={navbarIconButton}
+                                onClick={handleConfiguratorOpen}
+                            >
+                                <Icon>settings</Icon>
+                            </IconButton>
+                            <IconButton
+                                size="small"
+                                color="inherit"
+                                sx={navbarIconButton}
+                                aria-controls="notification-menu"
+                                aria-haspopup="true"
+                                variant="contained"
+                                onClick={handleOpenMenu}
+                            >
+                                <Icon className={light ? "text-white" : "text-dark"}>notifications</Icon>
+                            </IconButton>
+                            {renderMenu()}
+                        </SoftBox>
+                    </SoftBox>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 // Setting default values for the props of DashboardNavbar
 DashboardNavbar.defaultProps = {
-  absolute: false,
-  light: false,
-  isMini: false,
+    absolute: false,
+    light: false,
+    isMini: false,
 };
 
 // Typechecking props for the DashboardNavbar
 DashboardNavbar.propTypes = {
-  absolute: PropTypes.bool,
-  light: PropTypes.bool,
-  isMini: PropTypes.bool,
+    absolute: PropTypes.bool,
+    light: PropTypes.bool,
+    isMini: PropTypes.bool,
 };
 
 export default DashboardNavbar;
