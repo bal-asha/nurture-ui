28003e6856e464b65b9811f6f7eae590d8e403d4
Kalash432001 <kalashkimi@gmail.com>
2024-09-15 13:30:10 +0530

    Added Child Stage1 Form Fields

---
+++ src/child/child-registration/components/Stage1Info/data/data.js
@@ -0,0 +1,35 @@
+
+// Example data for dropdowns
+const stage1Data = {
+    gender: [
+      { value: "male", label: "Male" },
+      { value: "female", label: "Female" },
+    ],
+    childClassification: [
+      { value: "abandoned", label: "Abandoned / Family Not Traceable" },
+      { value: "orphaned", label: "Orphaned - No Guardians" },
+      { value: "financiallyDistressed", label: "Child having financially distressed parents and/or single Parent" },
+      // ... other options
+    ],
+    healthStatus: [
+      { value: "normal", label: "Normal" },
+      { value: "specialNeeds", label: "Special Needs" },
+      { value: "medicalCondition", label: "Medical Condition" },
+    ],
+    shelterHome: [
+      { value: "abc", label: "ABC - Aditya Birla Centre For The Welfare Of Children" },
+      { value: "abm", label: "ABM - The Children Aid Society Sanchalit Atirikt Balgruh Mulinche" },
+      // ... other options
+    ],
+    currentStatus: [
+      { value: "active", label: "Active" },
+      { value: "inactive", label: "Inactive" },
+    ],
+    yesNo: [
+      { value: "yes", label: "Yes" },
+      { value: "no", label: "No" },
+    ],
+  };
+
+  export default stage1Data;
+  
